5111f00838feecf1679d48c138cbdd64f9c15a93
GauravKhalase <gauravkhalase@gmail.com>
2024-02-29 19:49:44 +0530

    Added loop program

---
+++ 5_Loop_Statements/ForLoop1.java
@@ -0,0 +1,24 @@
+// Check leap year or not
+
+import java.util.Scanner;
+
+public class ForLoop1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year:");
+        int year = sc.nextInt();
+
+        if(year<1582)
+        {
+            System.out.println("Please enter year greater than 1582");   
+        }
+        else if((year%4 == 0 && year%100 != 0)|| year%400 == 0){
+            System.out.println("This is a leap year.");
+        }
+        else{
+            System.out.println("This is not a leap year.");
+        }
+
+        sc.close();
+    }   
+}
